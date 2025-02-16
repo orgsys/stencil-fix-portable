81838b469b51dc3e09e3cc69d311ffab91a05474
vitallyz <orgsys@gmail.com>
2025-02-16 19:09:06 +1100

    updated to suit my motor

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,67 +1,87 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+// #include <Adafruit_NeoPixel.h>  // Commented out NeoPixel
 #include <Servo.h>
 
-class LowPass {
-  private:
-    float _previousPrefilteredAnalog;
-    unsigned long _previousMicros = 0;
-  public:
-    float RCus;
-    LowPass(float _RCus) : RCus(_RCus) {}
-    float step(float prefilteredAnalog) {
-      if (_previousMicros == 0) {
-        _previousMicros = micros();
-        _previousPrefilteredAnalog = prefilteredAnalog;
-        return prefilteredAnalog;
-      }
-      unsigned long now = micros();
-      unsigned long dt = now - _previousMicros;
-      float alpha = dt / (RCus * dt);
-      _previousMicros = now;
-      _previousPrefilteredAnalog += alpha * (prefilteredAnalog - _previousPrefilteredAnalog);
-      return _previousPrefilteredAnalog;
+class LowPass
+{
+private:
+  float _previousPrefilteredAnalog;
+  unsigned long _previousMicros = 0;
+
+public:
+  float RCus;
+  LowPass(float _RCus) : RCus(_RCus) {}
+  float step(float prefilteredAnalog)
+  {
+    if (_previousMicros == 0)
+    {
+      _previousMicros = micros();
+      _previousPrefilteredAnalog = prefilteredAnalog;
+      return prefilteredAnalog;
     }
+    unsigned long now = micros();
+    unsigned long dt = now - _previousMicros;
+    float alpha = dt / (RCus * dt);
+    _previousMicros = now;
+    _previousPrefilteredAnalog += alpha * (prefilteredAnalog - _previousPrefilteredAnalog);
+    return _previousPrefilteredAnalog;
+  }
 };
 
-// How many internal neopixels do we have? some boards have more than one!
-#define NUMPIXELS 1
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+// Commented out all NeoPixel definitions
+// #define NUMPIXELS 1
+// Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 
-Servo servo;
+Servo esc; // renamed from servo to esc for clarity
 
 // low pass filter with an RC time constant of 40us
 LowPass lowPass(40);
 
-void setup() {
+// Constants for ESC control
+const int ARM_PULSE = 1000;   // ESC arming pulse
+const int START_PULSE = 1200; // Motor starts spinning
+const int MAX_PULSE = 2000;   // Maximum speed
+
+void setup()
+{
   Serial.begin(115200);
+  Serial.println("System starting...");
 
-#if defined(NEOPIXEL_POWER)
-  // If this board has a power control pin, we must set it to output and high
-  // in order to enable the NeoPixels. We put this in an #if defined so it can
-  // be reused for other boards without compilation errors
-  pinMode(NEOPIXEL_POWER, OUTPUT);
-  digitalWrite(NEOPIXEL_POWER, HIGH);
-#endif
+  // Removed NeoPixel power control code
+  // #if defined(NEOPIXEL_POWER)
+  //   pinMode(NEOPIXEL_POWER, OUTPUT);
+  //   digitalWrite(NEOPIXEL_POWER, HIGH);
+  // #endif
 
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels.setBrightness(127);
+  // Removed NeoPixel initialization
+  // pixels.begin();
+  // pixels.setBrightness(127);
 
-  pinMode(A2, INPUT);
-  analogReadResolution(12);
+  pinMode(A7, INPUT);
+  // Removed analogReadResolution as it's not available on Nano
+  // analogReadResolution(12);
 
-  servo.attach(A3, 1100, 1900);
-  servo.writeMicroseconds(1500);
+  esc.attach(11, ARM_PULSE, MAX_PULSE);
+  // esc.writeMicroseconds(ARM_PULSE);
+  // Serial.println("ESC initialized");
+
+  delay(1000);
 }
 
-void loop() {
-  int pot = analogRead(A2);
+void loop()
+{
+  int pot = analogRead(A7);
 
-  // map pot to an HSV value that is between 240 deg and 360 deg of the color wheel.
-  pixels.fill(pixels.ColorHSV(map(pot, 0, 4095, (pow(2, 16) - 1) * 240/360, pow(2, 16) - 1), 255, 255));
-  pixels.show();
+  // Direct mapping from pot to ESC range
+  int escValue = map(pot, 0, 1023, START_PULSE, MAX_PULSE);
+  float filteredValue = lowPass.step(escValue);
 
-  servo.writeMicroseconds(lowPass.step(map(pot, 0, 4095, 1200, 2000)));
+  // Simple debug output
+  Serial.print(pot);
+  Serial.print(" -> ");
+  Serial.println(filteredValue);
+
+  esc.writeMicroseconds(filteredValue);
 
   delay(20);
 }
